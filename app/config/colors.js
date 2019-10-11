c0d26a7187169a6cd2118a890d878cb75363d639
Guillermo Pérez <gperez78@gmail.com>
2019-10-11 12:22:50 -0300

    Colors config

---
+++ app/config/colors.js
@@ -0,0 +1,27 @@
+const navy = "#1c4865";
+const blue = "#1b77e2";
+const lightBlue = "#02a7e1";
+const black = "#333";
+const white = "#fff";
+const gray = "#999";
+
+const colors = {
+  primary: navy,
+  secondary: gray,
+  light: blue,
+  dark: navy,
+  text: {
+    primary: black,
+    secondary: gray,
+    highlighted: lightBlue,
+    dark: navy
+  },
+  bg: {
+    lighter: white,
+    light: blue,
+    dark: navy,
+    darker: "#444"
+  }
+};
+
+export default colors;
